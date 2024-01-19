cdc436b1ff14e442bd8b0cba124aa1e83e088fcb
ahmed abubakr <ahmedabubakr148@gmail.com>
2024-01-19 09:50:48 +0200

    deploy edit

---
+++ deploy.bat
@@ -0,0 +1,21 @@
+@echo off
+npm run build
+
+@REM # navigate into the build output directory
+cd dist
+
+@REM # if you are deploying to a custom domain
+@REM # echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+@REM # if you are deploying to https://<USERNAME>.github.io
+@REM # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+@REM # if you are deploying to https://<USERNAME>.github.io/<REPO>
+@REM # git push -f git@github.com:jjmax75/vue-gh-pages.git master:gh-pages
+git push -f
+
+@REM cd -
--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jjmax75/vue-gh-pages.git master:gh-pages
+# git push -f git@github.com:jjmax75/vue-gh-pages.git master:gh-pages
+git push -f
 
 cd -
